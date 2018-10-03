e99b8af7b2b7b49d09bc34f43e2a73459ae0577d
Durian <karen.kua7@gmail.com>
2018-10-03 04:08:49 -0400

    Finished routes for editing and associations

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -18,6 +18,15 @@ $(document).ready(() => {
         });
     };
 
+    function deleteArticle(idObj) {
+        $.ajax("/delete-article", {
+            type: "DELETE",
+            data: idObj
+        }).then(function(data) {
+            setTimeout(location.reload(), 1500);
+        });
+    };
+
     $("#scrapeBtn").on("click", event => {
         event.preventDefault();
         scrapeNews();
@@ -37,6 +46,70 @@ $(document).ready(() => {
         saveArticle(idObj);
     });
 
+    $(".delBtns").on("click", function(event) {
+        event.preventDefault();
+        console.log(`Hi the delete button is being clicked`);
+        console.log(`This is the ID stored in the button: ${$(this).val()}`);
+        var idObj = {
+            id: $(this).val()
+        }
+        deleteArticle(idObj);
+    });
 
+    $(".noteBtns").on("click", function(event) {
+        event.preventDefault();
+        $(".existingNotesContainer").empty();
+        $("#saveMsg").hide();
+        console.log("the note button is working");
+        let articleID = $(this).val();
+        $("#noteModal").attr("data-id", articleID);
+        console.log("the associated ID is: " + $("#noteModal").attr("data-id"));
+
+        $.ajax({
+            method: "GET",
+            url: "/articles/" + articleID
+          }).then(function(data) {
+              console.log(data);
+              $(".existingNotesContainer").append("<div id='existingNoteInput' name='body' contenteditable='true'></div>");
+              $(".existingNotesContainer").append("<button data-id='"+ data._id + "' class='saveNoteBtns'>Save</button>");
+              
+              if (data.note) {
+                  $("#existingNoteInput").append(data.note.body);
+                }
+                // $("#noteModal").modal("toggle");
+            })
+
+
+    });
+
+    $(document).on("click", ".saveNoteBtns", function (event) {
+        event.preventDefault();
+        console.log("Hi, the save button is working!");
+        let articleID = $(this).attr("data-id");
+        console.log(articleID);
+        console.log("input: " + $("#existingNoteInput").text());
+        // Run a POST request to change the note, using what's entered in the inputs
+        $.ajax({
+            method: "POST",
+            url: "/articles/" + articleID,
+            data: {
+                id: articleID,
+                // Value taken from note textarea
+                body: $("#existingNoteInput").text()
+            }
+        })
+            // With that done
+            .then(function (data) {
+                // Log the response
+                console.log(data);
+                // Empty the notes section
+                $("#saveMsg").show();
+                // $(".existingNotesContainer").empty();
+            });
+
+        // Also, remove the values entered in the input and textarea for note entry
+        // $("#titleinput").val("");
+        // $("#bodyinput").val("");
+    });
 
 });
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -29,7 +29,7 @@ router.get("/scrape", (req, res) => {
             // Create a new Article using the `result` object built from scraping
             avoidDupes(result);
         });
-        
+
         db.Article.find({ saved: false })
         .then(function (allArticles) {
             scrapedData = allArticles;
@@ -85,7 +85,7 @@ router.get("/scraped-results", (req, res) => {
     res.render("index", { scrapedItems: scrapedData });
 });
 
-router.put("/save-article", function (req, res) {
+router.put("/save-article", (req, res) => {
     db.Article.updateOne({ _id: req.body.id }, { $set: { saved: true } })
         .then(function (result) {
             console.log(`Saved article ${req.body.id}`);
@@ -99,6 +99,54 @@ router.get("/clear", (req, res) => {
     scrapedData = [];
     res.render("clear", { scrapedItems: scrapedData });
 });
+
+router.delete("/delete-article", (req, res) => {
+    console.log("Id: " + req.body.id);
+    db.Article.deleteOne({_id: req.body.id})
+    .then(function (result) {
+        console.log(`Deleted article ${req.body.id}`);
+        res.status(200).end();
+    }).catch(function (err) {
+        res.json(err);
+    });
+});
+
+router.get("/articles/:id", function(req, res) {
+    // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+    db.Article.findOne({ _id: req.params.id })
+      // ..and populate all of the notes associated with it
+      .populate("note")
+      .then(function(dbArticle) {
+        // If we were able to successfully find an Article with the given id, send it back to the client
+        res.json(dbArticle);
+      })
+      .catch(function(err) {
+        // If an error occurred, send it to the client
+        res.json(err);
+      });
+  });
+  
+  // Route for saving/updating an Article's associated Note
+  router.post("/articles/:id", function(req, res) {
+    // Create a new note and pass the req.body to the entry
+    db.Note.create(req.body)
+      .then(function(dbNote) {
+        // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+        // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+        // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+        return db.Article.findOneAndUpdate({ _id: req.params.id }, {note: dbNote._id }, { new: true });
+      })
+      .then(function(dbArticle) {
+        // If we were able to successfully update an Article, send it back to the client
+        res.json(dbArticle);
+      })
+      .catch(function(err) {
+        // If an error occurred, send it to the client
+        res.json(err);
+      });
+  });
+  
+
 
 
 export default router;
